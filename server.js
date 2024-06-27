f27009339dd853dbce2a4233db33adf9797d7fe1
Dulanmalinda <dulanmalinda@gmail.com>
2024-06-27 23:10:40 +0530

    sort method to upvotecount

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api', promptRoutes);
 
 // Add a route for the root path
 app.get('/', (req, res) => {
-  res.send('Welcome to the ARMOR API test');
+  res.send('Welcome to the ARMOR API');
 });
 
 const PORT = process.env.PORT || 5000;
