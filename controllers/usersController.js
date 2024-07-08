a54c3d949dd75cc984130df1c22574123939faf8
Dulanmalinda <dulanmalinda@gmail.com>
2024-07-08 19:34:32 +0530

    Username availability check

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -41,7 +41,25 @@ const getUserByWalletAddress = async (req, res) => {
   }
 };
 
+const checkUsernameAvailability = async (req, res) => {
+  const { fioUsername } = req.params;
+
+  try {
+    const user = await User.findOne({ fioUsername });
+
+    if (user) {
+      return res.status(200).json({ available: false });
+    }
+
+    res.status(200).json({ available: true });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
 module.exports = {
   saveUser,
-  getUserByWalletAddress
+  getUserByWalletAddress,
+  checkUsernameAvailability
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 // routes/userRoutes.js
 const express = require('express');
-const { saveUser, getUserByWalletAddress} = require('../controllers/usersController');
+const { saveUser, getUserByWalletAddress,checkUsernameAvailability } = require('../controllers/usersController');
 const router = express.Router();
 
 router.get('/user/:walletAddress', getUserByWalletAddress);
 router.post('/user/save', saveUser);
+router.get('/checkUsernameAvailability/:fioUsername', checkUsernameAvailability);
 
 module.exports = router;
